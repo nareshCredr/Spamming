f97a5516dbf1f12b42f4d36881e8e0a850dd6900
Naresh <naresh.k@credr.com>
2016-01-12 17:05:37 +0530

    uwsgi settings error fix

--- spamming/spamming/wsgi.py
+++ spamming/spamming/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os, sys, site
 
-SITE_DIR = '/home/ubuntu/My_Portfolio/'
+SITE_DIR = '/home/ubuntu/Spamming/spamming/'
 site.addsitedir(SITE_DIR)
 sys.path.append(SITE_DIR)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'spamming.settings'
